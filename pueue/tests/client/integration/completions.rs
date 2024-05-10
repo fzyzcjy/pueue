555edba358d8ed9fa90868bc140b9a4175de5b6f
Arne Beer <contact@arne.beer>
2024-05-10 04:25:31 +0200

    test: completions to stdout

--- pueue/tests/client/integration/completions.rs
+++ pueue/tests/client/integration/completions.rs
@@ -4,7 +4,7 @@ use anyhow::{Context, Result};
 use assert_cmd::prelude::*;
 use rstest::rstest;
 
-/// Make sure that the daemon's environment variables don't bleed into the spawned subprocesses.
+// Make sure the completion file generation succeeds.
 #[rstest]
 #[case("zsh")]
 #[case("elvish")]
@@ -18,6 +18,33 @@ fn autocompletion_generation(#[case] shell: &'static str) -> Result<()> {
         .arg("completions")
         .arg(shell)
         .arg("./")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .current_dir(env!("CARGO_TARGET_TMPDIR"))
+        .output()
+        .context(format!("Failed to run completion generation for {shell}:"))?;
+
+    assert!(
+        output.status.success(),
+        "Completion for {shell} didn't finish successfully."
+    );
+
+    Ok(())
+}
+
+// Make sure the completion file stream to stdout succeeds.
+#[rstest]
+#[case("zsh")]
+#[case("elvish")]
+#[case("bash")]
+#[case("fish")]
+#[case("power-shell")]
+#[case("nushell")]
+#[test]
+fn autocompletion_generation_stdout(#[case] shell: &'static str) -> Result<()> {
+    let output = Command::cargo_bin("pueue")?
+        .arg("completions")
+        .arg(shell)
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .current_dir(env!("CARGO_TARGET_TMPDIR"))
